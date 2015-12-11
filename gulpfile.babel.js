b4ae9594cba0b4dfd1272efa266bfb799bc4246f
Gabor Szabo <szabogabi@gmail.com>
2015-12-11 23:10:24 +0100

    owl szar

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -99,6 +99,7 @@ gulp.task('scripts', () =>
   // NOTE: The order here is important since it's concatenated in order from
   // top to bottom, so you want vendor scripts etc on top
   gulp.src([
+    'bower_components/owl.carousel/dist/owl.carousel.min.js',
     'src/assets/javascript/vendor.js',
     'src/assets/javascript/main.js'
   ])
