69a29a0d8b3179058b78347edd848c61b8d6ef6d
Gabor Szabo <szabogabi@gmail.com>
2015-12-11 01:05:02 +0100

    jolvan már

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,1 +1,13 @@
 console.log('It is main.js Hello world!');
+$('.feats').owlCarousel({
+    //rtl:true,
+    loop:true,
+    //margin:0,
+    //nav:false,
+    autoheight:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
